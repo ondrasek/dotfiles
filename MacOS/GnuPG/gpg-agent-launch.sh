c73e53cb2683ec0c8979ef1ce85069cabd1cf9d8
Ondrej Krajicek <ondrej.krajicek@ysoft.com>
2021-04-18 03:09:57 +0200

    Commit before gathering updated dotfiles.

--- MacOS/GnuPG/gpg-agent-launch.sh
+++ MacOS/GnuPG/gpg-agent-launch.sh
@@ -10,14 +10,13 @@ DEBUGFILE=/tmp/gpg-agent-launch.debug
 
 ## This is the randomly generated socket by launchctl. Difficult to override.
 SSH_AUTH_SOCK_SYMLINK=$SSH_AUTH_SOCK
-SSH_AUTH_SOCK=$($GPGCONF --list-dirs agent-ssh-socket)
+SSH_AUTH_SOCK_TARGET=$($GPGCONF --list-dirs agent-ssh-socket)
 rm -rf $SSH_AUTH_SOCK_SYMLINK
-ln -sf $SSH_AUTH_SOCK $SSH_AUTH_SOCK_SYMLINK
-export SSH_AUTH_SOCK_SYMLINK
+ln -sf $SSH_AUTH_SOCK_TARGET $SSH_AUTH_SOCK_SYMLINK
 
-
+rm  -f $DEBUGFILE
 echo $(date) | tee -a $DEBUGFILE
-echo SSH_AUTH_SOCK: $SSH_AUTH_SOCK | tee -a $DEBUGFILE
+echo SSH_AUTH_SOCK_TARGET: $SSH_AUTH_SOCK_TARGET | tee -a $DEBUGFILE
 echo SSH_AUTH_SOCK_SYMLINK: $SSH_AUTH_SOCK_SYMLINK | tee -a $DEBUGFILE
 $GPGCONF --list-dirs 2>&1 | tee -a $DEBUGFILE
 $GPG_CONNECT_AGENT killagent /bye 2>&1 | tee -a $DEBUGFILE

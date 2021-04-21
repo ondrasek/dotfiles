41522b0096cde7adfe9094a0dff4dee00b473425
Ondrej Krajicek <ondrej.krajicek@ysoft.com>
2021-04-21 08:08:00 +0200

    Commit before gathering updated dotfiles.

--- MacOS/GnuPG/gather.sh
+++ MacOS/GnuPG/gather.sh
@@ -5,7 +5,7 @@ cp -vf ~/bin/gpg-agent-launch.sh .
 cp -vf ~/.gnupg/gpg-agent.conf .
 cp -vf ~/.gnupg/gpg.conf .
 cp -vf ~/Library/LaunchAgents/gpg-agent.plist ../LaunchAgents
-cp -vf ~/Library/LaunchAgents/setenv.GNUPGHOME.plist .
-cp -vf ~/Library/LaunchAgents/setenv.GPG_MASTER_KEY_ID.plist .
-cp -vf ~/Library/LaunchAgents/setenv.SSH_AUTH_SOCK.plist .
-cp -vf ~/Library/LaunchAgents/setenv.TEST.plist .
+cp -vf ~/Library/LaunchAgents/setenv.GNUPGHOME.plist ../LaunchAgents
+cp -vf ~/Library/LaunchAgents/setenv.GPG_MASTER_KEY_ID.plist ../LaunchAgents
+cp -vf ~/Library/LaunchAgents/setenv.SSH_AUTH_SOCK.plist ../LaunchAgents
+cp -vf ~/Library/LaunchAgents/setenv.TEST.plist ../LaunchAgents

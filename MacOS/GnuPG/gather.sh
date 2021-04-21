c3b89429d76d1d4e76282852f1737c706108bec7
Ondrej Krajicek <ondrej.krajicek@ysoft.com>
2021-04-21 08:04:49 +0200

    Commit before gathering updated dotfiles.

--- MacOS/GnuPG/gather.sh
+++ MacOS/GnuPG/gather.sh
@@ -1,9 +1,10 @@
 git commit -a -m "Commit before gathering updated dotfiles."
 cp -vf ~/.ssh/config .
+ssh-add -L > id_rsa-yubikey.pub
 cp -vf ~/bin/gpg-agent-launch.sh .
 cp -vf ~/.gnupg/gpg-agent.conf .
 cp -vf ~/.gnupg/gpg.conf .
-cp -vf ~/Library/LaunchAgents/gpg-agent.plist .
+cp -vf ~/Library/LaunchAgents/gpg-agent.plist ../LaunchAgents
 cp -vf ~/Library/LaunchAgents/setenv.GNUPGHOME.plist .
 cp -vf ~/Library/LaunchAgents/setenv.GPG_MASTER_KEY_ID.plist .
 cp -vf ~/Library/LaunchAgents/setenv.SSH_AUTH_SOCK.plist .

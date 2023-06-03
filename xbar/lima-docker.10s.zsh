810a5012877fa94971f6a4f71ea5e1c1307aee88
Ondrej Krajicek <ondrej@krajicek.co>
2023-06-02 21:05:22 -0500

    Added xbar plugin for menubar item showing status of the lima VM running docker.

---
+++ xbar/lima-docker.10s.zsh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+
+# <xbar.title>lima docker vm status</xbar.title>
+# <xbar.version>v1.0</xbar.version>
+# <xbar.author>Ondrej Krajicek</xbar.author>
+# <xbar.author.github>ondrasek</xbar.author.github>
+# <xbar.desc>Shows lima docker VM status and CPU usage, with the option to start or stop it.</xbar.desc>
+# <xbar.image></xbar.image>
+# !xbar.dependencies>zsh</xbar.dependencies>
+
+NAME=docker
+VMNAME=VirtualMachine # Virtual Machine Service on MacOS
+LIMACTL=/opt/homebrew/bin/limactl
+
+STATUS=${$($LIMACTL list $NAME --format="{{.Status}}")##*( )}
+VMTYPE=${$($LIMACTL list $NAME --format="{{.VMType}}")##*( )}
+CPU=${$(ps -p $(pgrep $VMNAME) -o %cpu | tail -n 1)##*( )}
+
+if [[ $STATUS == *Running* ]]; then
+	echo $NAME "($VMTYPE) $CPU%"
+else
+	echo X
+fi

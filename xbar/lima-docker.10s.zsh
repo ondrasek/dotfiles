e013d2949dd37da6ee27e4064b2f7ff91a9794e4
Ondrej Krajicek <ondrej@krajicek.co>
2023-07-26 14:00:54 +0200

    Updated xbar script for lima.

--- xbar/lima-docker.10s.zsh
+++ xbar/lima-docker.10s.zsh
@@ -8,6 +8,19 @@
 # <xbar.image></xbar.image>
 # !xbar.dependencies>zsh</xbar.dependencies>
 
+function bytes_to_human() {
+  local -i bytes="$1"
+  local -a units=("KiB" "MiB" "GiB" "TiB" "PiB")
+  local -i unit_index=0
+
+  while (( bytes >= 1024 && unit_index < ${#units[@]} - 1 )); do
+    let "bytes /= 1024"
+    let "unit_index += 1"
+  done
+
+  printf "%d %s\n" "$bytes" "${units[$unit_index]}"
+}
+
 NAME=docker
 VMNAME=VirtualMachine # Virtual Machine Service on MacOS
 LIMACTL=/opt/homebrew/bin/limactl
@@ -15,16 +28,40 @@ CPUTHR=5.0
 
 STATUS=${$($LIMACTL list $NAME --format="{{.Status}}")##*( )}
 VMTYPE=${$($LIMACTL list $NAME --format="{{.VMType}}")##*( )}
+VMTYPE=$VMTYPE[1]
+CPUS=${$($LIMACTL list $NAME --format="{{.CPUs}}")##*( )}
+MEMORY=${$($LIMACTL list $NAME --format="{{.Memory}}")##*( )}
+DISK=${$($LIMACTL list $NAME --format="{{.Disk}}")##*( )}
+SSH=${$($LIMACTL show-ssh $NAME)##*( )}
+
+MEMORY=$(bytes_to_human $MEMORY)
+DISK=$(bytes_to_human $DISK)
+
+# If the VMTYPE is qemu, we need to check the CPU usage of the qemu process instead of VZ
+[[ $VMTYPE == *vz* ]] || VMNAME=qemu-system-aarch64
+
+# CPU Usage
 CPU=$(ps -p $(pgrep $VMNAME) -o %cpu | tail -n 1)
 
 if (( $CPU <= $CPUTHR )); then
-	CPU=""
+	SHORTCPU=""
 else
-	CPU=$CPU"%"
+	SHORTCPU=$CPU"%"
 fi
 
+[ -z $CPU ] || CPU=$CPU%
+
 if [[ $STATUS == *Running* ]]; then
-	echo $NAME "($VMTYPE)$CPU"
+	HEADER=$NAME"($VMTYPE)$SHORTCPU"
 else
-	echo X
+	HEADER=X
 fi
+
+echo $HEADER
+echo "---"
+echo VM Name: $VMNAME
+echo CPU Usage: $CPU
+echo CPUs: $CPUS
+echo Memory: $MEMORY
+echo Disk: $DISK
+
